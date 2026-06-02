994ccbc55b8a86587530ef7cfe080f004a0131ce
aneesh <aneeshyadav36@gmail.com>
2026-06-02 17:11:32 +0200

    adds post method

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
-
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+    
+my_post = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+           {"title": "title of post 2", "content": "content of post 2", "id": 2}]
 @app.get("/")
 def read_root():
     return {"Hello": "Aneesh"}
+
+
+@app.get("/posts")
+def get_data():
+    return {"data": my_post}
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_post.append(post_dict)
+    return {"data": post_dict}
